91b08c9a702d02d157cded444f6cf406227c8e83
dpankratov <dpankratov06@yandex.ru>
2022-11-08 16:58:50 +0300

    Ready app, but i need to rework it

---
+++ main_import.py
@@ -0,0 +1,11 @@
+from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from form import Ui_MainWindow
+import sys
+from time import sleep, time, ctime
+from pynput import mouse
+import threading
+from keyboard import read_hotkey, add_hotkey, remove_all_hotkeys, hook, unhook, press, release, remove_hotkey
+import os
+import re
+import webbrowser
